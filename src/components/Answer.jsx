36da3e1587e084929dc85c64b54e474137a2e83d
Kieran O'Connell <kieranpoconnell@gmail.com>
2023-01-24 23:51:38 -0330

    replaced classnames with styled-components

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,11 +1,28 @@
 import React from "react";
+import styled from "styled-components";
+import AnswerButton from "./Button";
+
+const ChoiceCtn = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  /* flex-direction: column; */
+  /* align-items: center; */
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  /* margin: 0.5em; */
+  border: 2px solid #f7f0f518;
+  border-radius: 8px;
+  background-color: #313e50;
+  cursor: pointer;
+`;
 
 const Answer = ({ data, confirm }) => {
-	return (
-		<div className="choice-card" onClick={confirm}>
-			<p className="choice">{data}</p>
-		</div>
-	);
+  return (
+    <ChoiceCtn onClick={confirm}>
+      <AnswerButton text={data} theme={"answerButton"}></AnswerButton>
+    </ChoiceCtn>
+  );
 };
 
 export default Answer;
--- src/components/TriviaWrapper.jsx
+++ src/components/TriviaWrapper.jsx
@@ -4,7 +4,37 @@ import Select from "./Select";
 import Input from "./Input";
 import Button from "./Button";
 import axios from "axios";
+import styled from "styled-components";
 
+const QuizForm = styled.form`
+  color: white;
+  font-size: calc(10px + 2vmin);
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 600;
+  margin: 0;
+  padding: 0;
+  select {
+    text-align: center;
+    width: auto;
+    padding: 0.5em;
+    margin: 1em;
+    font-size: 1em;
+    border: 1px solid #ccc;
+    border-radius: 0.25em;
+    /* box-shadow: 0 1px 0 1px rgba(0, 0, 0, 0.04);
+	-webkit-appearance: none;
+	-moz-appearance: none; */
+    /* appearance: none; */
+  }
+  label {
+    color: white;
+    font-size: calc(10px + 2vmin);
+    font-family: Arial, Helvetica, sans-serif;
+    font-weight: 600;
+    margin: 0;
+    padding: 0;
+  }
+`;
 const TriviaWrapper = () => {
   const [topic, setTopic] = useState("");
   const [question, setQuestion] = useState("");
@@ -42,7 +72,7 @@ const TriviaWrapper = () => {
     <>
       {showForm ? (
         <>
-          <form className="topic-form">
+          <QuizForm>
             <label htmlFor="topic">Pick a topic</label>
             <Select handleSelect={handleSelect} data={data} name="trivia" />
             <Input
@@ -50,12 +80,8 @@ const TriviaWrapper = () => {
               placeholder="Type your own.."
               setTopic={setTopic}
             />
-            <Button
-              className="submit-btn"
-              handleClick={getTopic}
-              text="Get question"
-            />
-          </form>
+            <Button handleClick={getTopic} text="Get question" />
+          </QuizForm>
         </>
       ) : // <Form handleClick={getTopic} data={data} label={"Pick a topic"} />
       null}
