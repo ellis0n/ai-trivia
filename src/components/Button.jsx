77fdb280c72d7a745770d18a3c57a1ed08923e83
glen may <ellisonglenmay@gmail.com>
2023-01-27 21:55:02 -0330

    changing colors and temporarily removing selects

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,36 +1,76 @@
 import React from "react";
 import styled from "styled-components";
+
 const StyledButton = styled.button`
-  display: block;
-  /* width: 100%; */
-  padding: 0.5em;
-  margin: 0.5em;
-  font-size: 0.7em;
-  border: 1px solid #ccc;
-  border-radius: 0.25em;
-  cursor: pointer;
-  color: ${(props) =>
-    props.buttonTheme === "answerButton" ? "blue" : "black"};
+	cursor: pointer;
+	color: ${({ theme }) => {
+		switch (theme) {
+			case "answer-btn":
+				return "#F4F3EE";
+			case "form-btn":
+				return "black";
+			default:
+				return "black";
+		}
+	}};
+	background-color: ${({ theme }) => {
+		switch (theme) {
+			case "answer-btn":
+				return "#8A817C";
+			case "form-btn":
+				return "white";
+			default:
+				return "red";
+		}
+	}};
+	border: none;
+	border-radius: 8px;
+	padding: 0.5em;
+	font-size: 1em;
+	font-weight: 600;
+
+	&:hover {
+		background-color: ${({ theme }) => {
+			switch (theme) {
+				case "answer-btn":
+					return "#8a817caa";
+				case "form-btn":
+					return "white";
+				default:
+					return "red";
+			}
+		}};
+		color: ${({ theme }) => {
+			switch (theme) {
+				case "answer-btn":
+					return "#F4F3EE";
+				case "form-btn":
+					return "black";
+				default:
+					return "black";
+			}
+		}};
+	}
 `;
 
 const Button = ({ text, handleClick, theme }) => {
-  const handleClickWithPrevent = (e) => {
-    e.preventDefault();
-    handleClick();
-  };
-  return (
-    <StyledButton
-      type="submit"
-      onClick={(e) => {
-        if (typeof handleClick === "function") {
-          handleClickWithPrevent(e);
-        }
-      }}
-      buttonTheme={theme}
-    >
-      {text}
-    </StyledButton>
-  );
+	const handleClickWithPrevent = (e) => {
+		e.preventDefault();
+		handleClick();
+	};
+	return (
+		<StyledButton
+			type="submit"
+			onClick={(e) => {
+				if (typeof handleClick === "function") {
+					handleClickWithPrevent(e);
+				}
+			}}
+			theme={theme}
+		>
+			{text}
+		</StyledButton>
+	);
 };
 
 export default Button;
